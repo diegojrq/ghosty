679b210f3ab48afa2a79b6cea2ee209715d56ee5
Diego Queiroz <diego.queiroz@economia.gov.br>
2023-05-10 19:37:12 -0300

    Commit inicial

---
+++ ghosty/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Point = System.Drawing.Point;
+
+namespace ghosty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            HWHEEL = 0x00001000,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public MouseEventFlags dwFlags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INPUT
+        {
+            public int type;
+            public MOUSEINPUT mi;
+        }
+
+        [Flags]
+        public enum Win32Consts
+        {
+            INPUT_MOUSE = 0,
+            INPUT_KEYBOARD = 1,
+            INPUT_HARDWARE = 2,
+        }
+
+        [DllImport("user32.dll")]
+        public static extern uint SendInput(
+            uint nInputs,
+            ref INPUT pInputs,
+            int cbSize);
+
+        [DllImport("user32.dll", SetLastError = false)]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public int dwTime;
+        }
+
+        static uint GetLastInputTime()
+        {
+            uint idleTime = 0;
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            uint envTicks = (uint)Environment.TickCount;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                uint lastInputTick = (uint)lastInputInfo.dwTime;
+
+                idleTime = envTicks - lastInputTick;
+            }
+
+            return ((idleTime > 0) ? (idleTime / 1000) : 0);
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            // code goes here
+            MoveMouse();
+        }
+
+
+        private void MoveMouse() {
+            int distance = 200;
+            int speed = 2;
+
+
+            for (int i = 0; i < distance; i++)
+            {
+                Point point = new Point(0, -1);
+
+                try
+                {
+                    var mi = new MOUSEINPUT
+                    {
+                        dx = point.X,
+                        dy = point.Y,
+                        mouseData = 0,
+                        time = 0,
+                        dwFlags = MouseEventFlags.MOVE,
+                        dwExtraInfo = UIntPtr.Zero
+                    };
+                    var input = new INPUT
+                    {
+                        mi = mi,
+                        type = Convert.ToInt32(Win32Consts.INPUT_MOUSE)
+                    };
+                    SendInput(1, ref input, Marshal.SizeOf(input));
+                }
+                catch (Exception ex)
+                {
+                    //StaticCode.Logger?.Here().Error(ex.Message);
+                }
+
+                Thread.Sleep(speed);
+            }
+            lbl.Content = GetLastInputTime().ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Start();
+        }
+    }
+}
